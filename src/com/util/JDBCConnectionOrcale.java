9baeab5aba003826673e432638d8b65e65c625d7
Jap Purohit <pjap041@gmail.com>
2021-02-28 12:02:37 +0530

    JDBC Connection done

---
+++ src/com/util/JDBCConnectionOrcale.java
@@ -0,0 +1,28 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCConnectionOrcale {
+	public static Connection connectionMethod() {
+		String driverName = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:ORCL";
+		String userName= "dbms_project";
+		String password= "dbms_project";
+		Connection con = null;
+		try {
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url,userName,password);
+			if(con == null)
+				System.out.println("not connected");
+			else
+				System.out.println("connected");
+			System.out.println(con);
+		}
+		catch(Exception e){
+			System.out.println("Something went wrong "+e);
+		}
+		return con;
+	}
+	
+}
