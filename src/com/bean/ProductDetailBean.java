2520893c29c99c76c70474dd6c2fc40a98c95ec2
Jap Purohit <pjap041@gmail.com>
2021-03-22 17:22:07 +0530

    Admin Panel design Done, Bean and Dao Creation Done.

---
+++ src/com/bean/ProductDetailBean.java
@@ -0,0 +1,80 @@
+package com.bean;
+
+public class ProductDetailBean {
+	private int productId, categoryId, subCategoryId,quantity,isActive;
+	private float originalPrice,offerPrice;
+	private String productName, companyName, offerTill,imagePath,productDescription;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public int getSubCategoryId() {
+		return subCategoryId;
+	}
+	public void setSubCategoryId(int subCategoryId) {
+		this.subCategoryId = subCategoryId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+	public float getOriginalPrice() {
+		return originalPrice;
+	}
+	public void setOriginalPrice(float originalPrice) {
+		this.originalPrice = originalPrice;
+	}
+	public float getOfferPrice() {
+		return offerPrice;
+	}
+	public void setOfferPrice(float offerPrice) {
+		this.offerPrice = offerPrice;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getOfferTill() {
+		return offerTill;
+	}
+	public void setOfferTill(String offerTill) {
+		this.offerTill = offerTill;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+}
